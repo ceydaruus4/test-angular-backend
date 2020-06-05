9bef29e1403111918dbea7ea2cf8914cc7dda21c
ceydaruus4 <ceydaruus0@gmail.com>
2020-06-05 22:11:08 +0100

    new

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+let allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+};
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const port = 8080;
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ test.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(){
+    this.name = "idarous";
+    this.age = 24;
+  }
+  
+  printDetails(){
+    console.log(this.name, this.age);
+  }
+}
+
+module.exports = Person;
+
